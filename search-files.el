1edc148cc9b10f1d65395c59459b78a8f572fe08
Dan Davison <dandavison7@gmail.com>
2016-07-11 22:51:24 -0700

    Split filter-results mode into separate project

--- search-files.el
+++ search-files.el
@@ -1,5 +1,5 @@
 ;;; search-files.el --- search files using `git grep` or other backends
-;; Version: 0.0.0
+;; Version: 0.0.1
 ;; Author: dandavison7@gmail.com
 ;; Package-Requires: ((projectile "0.10.0"))
 ;; Keywords: search, git, projectile
@@ -7,30 +7,11 @@
 
 (require 'cl)
 (require 'projectile)
+(require 'filter-results)  ;; https://github.com/dandavison/emacs-filter-results
 
 (defvar search-files-ag-arguments '("-z"))
 
 (defvar search-files-results-buffer-name "*search*")
-
-(defvar search-files-mode-map (make-sparse-keymap))
-
-(defvar search-files-max-line-width 300
-  "Output lines will be truncated to this width to avoid slowing down emacs")
-
-(define-derived-mode search-files-mode
-  compilation-mode "search-files"
-  "Major mode for search-files results buffer.
-\\{search-files-mode-map}"
-
-  (add-hook 'compilation-finish-functions
-            'search-files-clean-up-compilation-buffer nil t))
-
-(define-key search-files-mode-map "/" 'search-files-filter-results)
-(define-key search-files-mode-map [(control /)] 'search-files-undo)
-(define-key search-files-mode-map "\C-k" 'search-files-kill-line)
-(define-key search-files-mode-map "\C-_" 'search-files-undo)
-(define-key search-files-mode-map "\C-xu" 'search-files-undo)
-(define-key search-files-mode-map [(super z)] 'search-files-undo)
 
 ;;;###autoload
 (defun search-files-read-from-minibuffer (&optional search-for-definition-p)
@@ -62,30 +43,6 @@
    (read-from-minibuffer "File name regex: ")
    (projectile-project-root)
    'name))
-
-(defun search-files-filter-results (&optional arg)
-  "Filter search results, retaining matching lines.
-
-With prefix argument, retain non-matching lines."
-  (interactive "P")
-  (search-files-do-in-results-buffer
-   (if arg 'delete-matching-lines 'delete-non-matching-lines) 'from-beginning))
-
-(defun search-files-kill-line ()
-  "Kill line in search results buffer"
-  (interactive)
-  (search-files-do-in-results-buffer 'kill-line))
-
-(defun search-files-undo ()
-  "Undo in search results buffer"
-  (interactive)
-  (search-files-do-in-results-buffer 'undo))
-
-(defun search-files-do-in-results-buffer (fn &optional do-from-beginning)
-  (let ((buffer-read-only nil))
-    (save-excursion
-      (when do-from-beginning (goto-char (point-min)))
-      (call-interactively fn))))
 
 (defun search-files-project-backend ()
   "Return search backend for project.
@@ -127,20 +84,20 @@ With prefix argument, retain non-matching lines."
     (delete-region (point-min) (point-max))
     (save-excursion
       (insert (search-files-do-search string backend))))
-  (search-files-mode)
+  (filter-results-mode)
   (when (eq (count-lines (point-min) (point-max)) 1)
     (compile-goto-error)))
 
 (defun search-files-do-search (string backend)
   (case backend
     ((ag git-grep)
-     (search-files-truncate-lines
+     (filter-results-truncate-lines
       (shell-command-to-string
        (search-files-make-search-command string backend))))
     ('name
      (replace-regexp-in-string
       "$" ":1:"
-      (search-files-truncate-lines
+      (filter-results-truncate-lines
        (shell-command-to-string
         (format "git ls-files | grep '%s'" string)))))
     (t (error "Invalid backend"))))
@@ -157,45 +114,6 @@ With prefix argument, retain non-matching lines."
       (list "git" "grep" "-n" "--exclude-standard" "--no-index" string))
      (t (error "Invalid backend")))
    " "))
-
-(defun search-files-truncate-lines (string)
-  "Truncates lines to `search-files-max-line-width'"
-  (mapconcat
-   (lambda (line) (truncate-string-to-width line search-files-max-line-width))
-   (split-string string "[\n\r]+")
-   "\n"))
-
-(defun search-files-clean-up-compilation-buffer (&optional buf ignored)
-  (with-current-buffer (or buf (current-buffer))
-    (let ((buffer-read-only nil)
-          (grep-match-re "^[^: ]+:[0-9]+:"))
-      (save-excursion
-        (goto-char (point-min))
-        (delete-region (point)
-                       (progn
-                         (re-search-forward grep-match-re)
-                         (point-at-bol)))
-        (goto-char (point-max))
-        (delete-region (progn
-                         (re-search-backward grep-match-re)
-                         (forward-line 1)
-                         (point-at-bol))
-                       (point))))))
-
-(require 'helm)
-
-(defvar helm-filter-mode-buffer "*helm filter*")
-
-(defun helm-filter-mode-action (-ignored)
-  (switch-to-buffer helm-filter-mode-buffer)
-  (let ((buffer-read-only nil))
-    (delete-region (point-min) (point-max))
-    (insert
-     (with-current-buffer helm-last-buffer
-       (search-files-truncate-lines (buffer-string))))
-    (goto-char (point-min))
-    (search-files-clean-up-compilation-buffer)
-    (search-files-mode)))
 
 (provide 'search-files)
 ;;; search-files.el ends here
